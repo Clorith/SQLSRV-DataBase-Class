c9b08e72c726507181c819b29ce695e4badd49cf
Davide Permunian <davide@davidepermunian.com>
2018-03-26 19:13:45 +0200

    Fix error log by returning print_r instead of print

--- db.php
+++ db.php
@@ -192,7 +192,7 @@ class SQLSRV_DataBase {
 			 */
 			$error = sqlsrv_errors();
 			$this->error[] = $error;
-			error_log( 'Database failure: ' . print_r($error) );
+			error_log( 'Database failure: ' . print_r($error, true) );
 			return false;
 		}
 		sqlsrv_configure( 'WarningsReturnAsErrors', true );
